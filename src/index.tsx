e5a26b2b3e3841194e5d69c4e325b6cd1198ce2f
Hennadii Shevchenko <shege68@gmail.com>
2023-05-18 20:15:20 +0300

    JSX повертає завжди один елемент

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// React Element
 const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+console.log('🚀 ~ file: index.tsx:6 ~ title:', title)
+
+// JSX
+const app = (
+    <div>
+        <h1 id="text">Hello App</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            possimus ratione nihil eos ipsam! Officia delectus unde corporis
+            tempora adipisci fuga est aspernatur quia porro accusantium odio,
+            aperiam ipsum velit!
+        </p>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
