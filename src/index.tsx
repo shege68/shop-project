26b49b06402aba989575d475014822888122ee03
Hennadii Shevchenko <shege68@gmail.com>
2023-05-18 20:43:26 +0300

    React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const Title = () => {
+    return <h1>Hello App component</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                ipsum doloribus error alias magni, sint expedita non soluta
+                minus laborum harum eligendi ea optio repudiandae autem nulla!
+                Nulla, corrupti nam!
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex
+                iste, facilis voluptatum ducimus tempore, sequi nostrum quos,
+                numquam hic adipisci deserunt nobis nihil libero ipsam molestias
+                labore nam quae iusto?
+            </p>
+        </React.Fragment>
+    )
+}
+
 const App = () => {
     return (
         <div>
-            <h1>Hello App component</h1>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-                ab non, nulla ad possimus nihil eveniet delectus ratione fugiat
-                nam reiciendis a minus eius doloribus porro voluptatem
-                voluptatum corporis voluptates.
-            </p>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Beatae, in ipsum impedit totam iure nobis fugiat! Modi, hic. At
-                reiciendis voluptas natus aut eveniet id odio debitis vitae.
-                Dolore, nulla.
-            </p>
+            <Title />
+            <Content />
         </div>
     )
 }
