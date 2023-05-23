613eb628214b8fbad01091e67242b5a402dcf2b6
Hennadii Shevchenko <shege68@gmail.com>
2023-05-23 19:23:54 +0300

    деструктуризація

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ type TitleProps = {
     title: string
 }
 
-const Title = (props: TitleProps) => {
-    return <h1>Hello {props.title} component</h1>
+const Title = ({ title }: TitleProps) => {
+    return <h1>Hello {title} component</h1>
 }
 
 const Content = () => {
