fd2fb64101dc6227d6d15dd6bf8bec0bc9162be2
Hennadii Shevchenko <shege68@gmail.com>
2023-05-25 20:32:26 +0300

    зміна кольору з CSS injection order

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -10,13 +10,8 @@ import Menu from 'components/Menu/Menu'
 
 type Props = {}
 const Header = (props: Props) => {
-    const appBar = true
     return (
-        <AppBar
-            position="static"
-            className="app-bar"
-            sx={{ backgroundColor: appBar ? 'green' : 'purple' }}
-        >
+        <AppBar position="static" className="app-bar">
             <Container>
                 <Toolbar>
                     <IconButton
