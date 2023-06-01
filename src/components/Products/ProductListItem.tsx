cd22badc961e677b7866e289572d9df649e6a60a
Hennadii Shevchenko <shege68@gmail.com>
2023-06-01 21:05:30 +0300

    прив'язали this через стрілкову функцію і state

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -21,17 +21,19 @@ type State = {
     count: number
 }
 class ProductListItem extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props)
-        this.state = {
-            count: 1,
-        }
-        this.onIncrementClick = this.onIncrementClick.bind(this)
+    state = {
+        count: 1,
     }
 
-    onIncrementClick() {
+    onIncrementClick = () => {
         this.setState((prevState) => ({
             count: prevState.count + 1,
+        }))
+    }
+
+    onDecrementClick = () => {
+        this.setState((prevState) => ({
+            count: prevState.count - 1,
         }))
     }
 
@@ -51,11 +53,16 @@ class ProductListItem extends React.Component<Props, State> {
                     </div>
                     <div className="product-price">Price: $ {price}</div>
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={() => this.onDecrementClick()}
+                        >
+                            -
+                        </Button>
                         <TextField value={this.state.count} size="small" />
                         <Button
                             variant="outlined"
-                            onClick={this.onIncrementClick}
+                            onClick={() => this.onIncrementClick()}
                         >
                             +
                         </Button>
