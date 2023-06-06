53651b449ae1777d54a0e66b21757ca72c3f7ede
Hennadii Shevchenko <shege68@gmail.com>
2023-06-06 19:36:06 +0300

    додали лічильники через Props

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -5,6 +5,7 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
+import { useState } from 'react'
 import './ProductListItem.scss'
 
 type Props = {
@@ -24,6 +25,16 @@ const ProductListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrementClick = () => {
+        setCount((prevState) => prevState + 1)
+    }
+
+    const onDecrementClick = () => {
+        setCount((prevState) => prevState - 1)
+    }
+
     return (
         <Card className="product" variant="outlined">
             <CardContent>
@@ -36,9 +47,21 @@ const ProductListItem = ({
                 <div className="product-features">Capacity: {capacity}Gb</div>
                 <div className="product-price">Price: $ {price}</div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField value="1" size="small" />
-                    <Button variant="outlined">+</Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => onDecrementClick()}
+                        disabled={count <= 1}
+                    >
+                        -
+                    </Button>
+                    <TextField value={count} size="small" />
+                    <Button
+                        variant="outlined"
+                        onClick={() => onIncrementClick()}
+                        disabled={count >= 10}
+                    >
+                        +
+                    </Button>
                 </div>
                 <CardActions className="btn-wrap">
                     <Button variant="outlined">Add to cart</Button>
