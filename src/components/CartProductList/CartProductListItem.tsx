3e9797ea3abf648684de7f9d08401764c47e5022
Hennadii Shevchenko <shege68@gmail.com>
2023-06-15 00:12:06 +0300

    вивели продукти в cart

---
+++ src/components/CartProductList/CartProductListItem.tsx
@@ -0,0 +1,13 @@
+import { Product } from 'utils/productsArray'
+
+type Props = { product: Product; productCount: number }
+
+const CartProductListItem = ({ product, productCount }: Props) => {
+    return (
+        <div>
+            {product.title}: {productCount}
+        </div>
+    )
+}
+
+export default CartProductListItem
--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -1,5 +1,7 @@
 import CartTotal from 'components/CartTotal/CartTotal'
 import CartProductList from 'components/CartProductList/CartProductList'
+import CartProductListItemExtended from 'components/CartProductList/CartProductListItemExtended'
+import { Grid } from '@mui/material'
 
 type Props = {
     productsInCart: {
@@ -10,8 +12,13 @@ type Props = {
 const CartPage = ({ productsInCart }: Props) => {
     return (
         <div>
-            <CartProductList productsInCart={productsInCart} />
-            <CartTotal productsInCart={productsInCart} />
+            <Grid container spacing={4}>
+                <CartProductList
+                    productsInCart={productsInCart}
+                    CartItem={CartProductListItemExtended}
+                />
+                <CartTotal productsInCart={productsInCart} />
+            </Grid>
         </div>
     )
 }
