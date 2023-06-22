7b1a133490999dd767f4581c6a22b526bb9917d0
Hennadii Shevchenko <shege68@gmail.com>
2023-06-22 19:00:59 +0300

    добавили LikeSlice

--- src/redux/likeReducer.ts
+++ src/redux/likeReducer.ts
@@ -1,4 +1,5 @@
-import { AnyAction } from 'redux'
+import { createSlice } from '@reduxjs/toolkit'
+
 
 type LikeState = {
     [id: number]: boolean
@@ -6,15 +7,29 @@ type LikeState = {
 
 const initialState: LikeState = { 1: true, 2: true }
 
-export const likeReducer = (state = initialState, action: AnyAction) => {
-    switch(action.type) {
-        case "TOGGLE_LIKE": {
-            return {
-                ...state,
-                [action.id]: !state[action.id]
-            }
-        }
-        default:
-            return state
+// export const likeReducer = (state = initialState, action: AnyAction) => {
+//     switch(action.type) {
+//         case "TOGGLE_LIKE": {
+//             return {
+//                 ...state,
+//                 [action.id]: !state[action.id]
+//             }
+//         }
+//         default:
+//             return state
+//     }
+// }
+
+export const LikeSlice = createSlice({
+    name: "like",
+    initialState,
+    reducers: {
+        toggleLike : (state, action) => ({
+            ...state,
+            [action.payload]: !state[action.payload]
+        })
     }
-}
+})
+
+export const {toggleLike} = LikeSlice.actions
+export default LikeSlice.reducer
